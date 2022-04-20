8ae8a27c6e262763d949fe92bcf44fe903b3bbd4
TirusTrinity <teis.jonsen@gmail.com>
2022-04-20 15:31:35 +0200

    Initial commit

---
+++ minmax/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace minmax
+{
+    class Program
+    {
+        public static string userInput;
+        public static string[] split;
+        public static List<Int32> ints = new();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv komma seperede tal værdier");
+            userInput = Console.ReadLine();
+            split = userInput.Split(',');
+            for (int i = 0; i < split.Length; i++)
+            {
+                ints.Add(Int32.Parse(split[i]));
+            }
+
+            ints.Sort();
+            Console.WriteLine(ints[ints.Count-1] );
+            Console.ReadKey();
+        }
+    }
+}
